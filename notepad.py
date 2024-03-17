d8e781c422f99ee2490f21039148f7e2447436b8
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-16 21:39:03 -0500

    Add save_file and open_file functions

--- notepad.py
+++ notepad.py
@@ -8,6 +8,12 @@ root.title('Notepy')
 root.config(bg='black')
 root.resizable(False,False)
 
+def save_file():
+        print('')
+
+def open_file():
+        print('')
+
 
 b1 = Button(root,width='20',height='2',bg='#fff',text='save file',command=save_file).place(x=100,y=5)
 b2 = Button(root,width='20',height="2",bg='#fff',text='open file',command=open_file).place(x=300,y=5)
