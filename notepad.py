1f8cc91005e766e608244764aee395bf2cfb4a1d
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-16 21:37:10 -0500

    Set up basic window configuration for Notepy application including window size, title, background color, and disabling resizing.

--- notepad.py
+++ notepad.py
@@ -3,7 +3,14 @@ from tkinter import filedialog
 
 
 root = Tk()
-root.geometry("600x600")
-root.title("Notepy")
+root.geometry('600x600')
+root.title('Notepy')
 root.config(bg='black')
 root.resizable(False,False)
+
+
+b1 = Button(root,width='20',height='2',bg='#fff',text='save file',command=save_file).place(x=100,y=5)
+b2 = Button(root,width='20',height="2",bg='#fff',text='open file',command=open_file).place(x=300,y=5)
+
+
+root.mainloop()
