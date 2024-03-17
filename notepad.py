70b29211e70463b0e5e2d26763b15c3b30b5572d
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-16 21:53:15 -0500

    Finished V.1

--- notepad.py
+++ notepad.py
@@ -8,15 +8,28 @@ root.title('Notepy')
 root.config(bg='black')
 root.resizable(False,False)
 
+
 def save_file():
-        print('')
+        open_file=filedialog.asksaveasfile(mode='w',defaultextension='.txt')
+        if open_file is None:
+                return
+        text=str(entry.get(1.0,END))
+        open_file.write(text)
+        open_file.close()
 
 def open_file():
-        print('')
+       file=filedialog.askopenfile(mode='r',filetype=[('text files','*.txt')])
+       if file is not None:
+               content=file.read()
+               entry.insert(INSERT.content)
 
 
 b1 = Button(root,width='20',height='2',bg='#fff',text='save file',command=save_file).place(x=100,y=5)
 b2 = Button(root,width='20',height="2",bg='#fff',text='open file',command=open_file).place(x=300,y=5)
+
+entry=Text(root, height='33',width='82',wrap=WORD, font=('Times New Roman', 18))
+entry.place(x=10,y=60)
+
 
 
 root.mainloop()
